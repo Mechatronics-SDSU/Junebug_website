0d03fa67c5f45edb54a19c11683c5c9abd8b0afb
TjRichmond <tjrichmond99@gmail.com>
2022-03-21 23:04:41 -0700

    restructed folder structure and added backend section

---
+++ junebug-web/frontend/src/components/Pages/Order.jsx
@@ -0,0 +1,25 @@
+import { Card } from ".."
+
+function Order() {
+    return (
+        <div className="order">
+            <h1>Available Restaurants</h1>
+            <div className="card-container">
+                <Card title="Rubio's" body="This is a test" />
+                <Card title="Panda Express" body="This is a test" />
+                <Card title="Chipotle" body="This is a test" />
+                <Card title="Rubio's" body="This is a test" />
+                <Card title="Panda Express" body="This is a test" />
+                <Card title="Chipotle" body="This is a test" />
+                <Card title="Rubio's" body="This is a test" />
+                <Card title="Panda Express" body="This is a test" />
+                <Card title="Chipotle" body="This is a test" />
+                <Card title="Rubio's" body="This is a test" />
+                <Card title="Panda Express" body="This is a test" />
+                <Card title="Chipotle" body="This is a test" />
+            </div>
+        </div>
+    );
+}
+
+export default Order;
