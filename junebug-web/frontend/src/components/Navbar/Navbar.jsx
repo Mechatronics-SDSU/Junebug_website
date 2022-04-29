d01fcf00083537e53c90b81ad3f1bc57cd9c82f3
TjRichmond <tjrichmond99@gmail.com>
2022-04-29 13:08:24 -0700

    Responsive navbar to resizable window

--- junebug-web/frontend/src/components/Navbar/Navbar.jsx
+++ junebug-web/frontend/src/components/Navbar/Navbar.jsx
@@ -3,10 +3,9 @@ import "./navbar.css";
 import { Button } from "..";
 import { ReactComponent as Logo } from "../../assets/mech_logo.svg"
 
-function Navbar() {
+function Navbar({token}) {
   return (
     <nav className="navbar">
-      <div className="navbar-container">
         <NavLink className="navbar-logo" to="/Junebug_website">
           <Logo height="75px" width="80px" align="left" />
           <text className="navbar-logo-text">Junebug</text>
@@ -14,10 +13,9 @@ function Navbar() {
         <div className="navbar-btn">
           <Button name="RESTAURANTS" route="/Junebug_website/restaurants" />
           <Button name="CART" route="/Junebug_website/cart" />
-          <Button name="Sign In" route="/Junebug_website/login" />
-          <Button name="Sign Up" route="/Junebug_website/signup" />
+          <Button name="LOGIN" route="/Junebug_website/login" />
+          <Button name="SIGN UP" route="/Junebug_website/signup" />
         </div>
-      </div>
     </nav>
   );
 }
