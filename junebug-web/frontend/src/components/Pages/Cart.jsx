8081b1ee5cfdac9c177649143348bcebbe28a87f
Eli-Reece <63837741+Eli-Reece@users.noreply.github.com>
2022-05-05 01:23:23 -0700

    pre-demo eli commit

--- junebug-web/frontend/src/components/Pages/Cart.jsx
+++ junebug-web/frontend/src/components/Pages/Cart.jsx
@@ -8,7 +8,6 @@ function Cart() {
 
     const cartItems = useContext(CartStateContext);
     const dispatch = useContext(CartDispatchContext);
-
     const navigate = useNavigate();
 
     const handleRemove = (itemRemove) => {
@@ -18,8 +17,8 @@ function Cart() {
     const handleTotal = (itemPrice) => {
     }
 
-    const handleCheckout = () =>  {
-        navigate('/Junebug_website/checkout',{replace: true});
+    const handleCheckout = () => {
+        navigate('/Junebug_website/checkout', { replace: true });
     }
 
     return (
@@ -45,11 +44,38 @@ function Cart() {
                     );
                 })}
             </ul>
+            <div className="checkcont">
+                <div className="row">
+                    <h3>Billing Address and Delivery Info</h3>
+                    <label for="fname"><i class="fa fa-user"></i> First Name </label>
+                    <input type="text" id="fname" name="fname" placeholder="John"/><br></br>
+                    <label for="lname"><i class="fa fa-user"></i> Last Name </label>
+                    <input type="text" id="lname" name="lname" placeholder="Doe"/><br></br>
+                    <label for="email"><i class="fa fa-envelope"></i> Email </label>
+                    <input type="text" id="email" name="email" placeholder="john@example.com"/><br></br>
+                    <label for="adr"><i class="fa fa-address-card-o"></i> Address </label>
+                    <input type="text" id="adr" name="address" placeholder="542 W. 15th Street"/><br></br>
+                    <label for="city"><i class="fa fa-institution"></i> City </label>
+                    <input type="text" id="city" name="city" placeholder="New York"/><br></br>
+                    <label for="destination">Choose Delivery Location: </label>
+                    <select name="destination" id="destination">
+                        <option value="Storm Hall">Storm Hall</option>
+                        <option value="Love Library">Love Library</option>
+                        <option value="GMCS">GMCS</option>
+                        <option value="Student Union">Student Union</option>
+                        <option value="Viejas">Viejas</option>
+                    </select> <br></br><br></br>
+                    <label for="cardnum">Enter Card Number: </label>
+                    <input type="text" id="cardnum" name="cardnum"/><br></br>
+                    <label for="secNum">Enter Security Number: </label>
+                    <input type="text" id="secNum" name="secNum"/><br></br>
+                </div>
+            </div>    
             <div className="cart-checkout">
                 <h2>
                     Order Total: ${orderTotal}
                 </h2>
-                <button 
+                <button
                     className="cart-checkout-btn"
                     onClick={handleCheckout}
                 >Checkout</button>
