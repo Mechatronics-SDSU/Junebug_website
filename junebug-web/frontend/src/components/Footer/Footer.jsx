bbcd8784078a76f8abb0eeeb60b839119a42f403
TjRichmond <tjrichmond99@gmail.com>
2022-05-11 13:57:12 -0700

    Updated footer styling

--- junebug-web/frontend/src/components/Footer/Footer.jsx
+++ junebug-web/frontend/src/components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ import { NavLink } from "react-router-dom";
 function Footer() {
     return (
         <footer className="footer">
-            <div className="container">
+            <div className="footer-container">
                 <NavLink className="nav-link" to="/Junebug_website/about">
                     About
                 </NavLink>
-                <h4>Contact Us: <u>mechatronics.sdsu@gmail.com</u></h4>
-                <p>Copyright &copy; SDSU Mechatronics 2022</p>
+                <div className="contact">Contact Us: <u>mechatronics.sdsu@gmail.com</u></div>
+                <div className="contact">Copyright &copy; SDSU Mechatronics 2022</div>
             </div>
         </footer>
     );
